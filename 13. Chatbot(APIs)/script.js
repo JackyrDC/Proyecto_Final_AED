c731e34a36068efe206866387bec6dfb0405209c
JackyrDC <djackyrcalix@gmail.com>
2022-12-10 23:30:52 -0600

    Final
    Posibles cambios a futuro: terminar el servicio de clima

--- 13. Chatbot(APIs)/script.js
+++ 13. Chatbot(APIs)/script.js
@@ -126,15 +126,42 @@ class ChatTree {
             let data;
             if(this.chat_tree['type']==="function"){
                 // console.log(String(this.chat_tree['message']),String("getJoke()"));
-                if(this.chat_tree['message']==="getJoke()"){
+                if(this.chat_tree['message']==="getJokeSP()" || this.chat_tree['message']==="getJokeEN()"){
                     data = await eval(this.chat_tree['message']);
-                    data = data.value.joke;
-                } else{
+                    data = data.joke;
+                } else if(this.chat_tree['message']==="getNewsBC()"||this.chat_tree['message']==="getNewsSC()" || this.chat_tree['message']==="getNewsQatar()"){
                     data = await eval(this.chat_tree['message']);
-                    data = data.articles[0].image+"<br>"+data.articles[0].title+"<br>"+data.articles[0].description+"<br><br>"+data.articles[0].url;
+                    var tam=data.articles.length-1;
+                    var rand = Math.floor(Math.random() * tam);
+                    try{data = "<img src='"+data.articles[rand].urlToImage+"' width='85%'></img><br>"+data.articles[rand].title+"<br>"+data.articles[rand].description+"<br><br>"+"<a href='"+data.articles[rand].url+"'>"+data.articles[rand].url+"</a>";
+                    }
+                    catch (e) {
+                        data=data.articles[rand].title+"<br>"+data.articles[rand].description+"<br><br>"+data.articles[rand].url;
+                    }
                 }
             } else{
-                data = this.chat_tree['message'];
+                data = await this.chat_tree['message'];
+                if(data==="getWeatherTd()"){
+                    try{
+                        data=await eval(this.chat_tree['message']);
+                        let condi=data.current.condition.text;
+                        let condi_img=data.current.condition.icon;
+                        let temp=data.current.temp_c;
+                        data = condi_img+"Condition: "+condi+"<br>"+"Temperature: "+temp+"°C<br>";
+                    } catch (e) {
+                        data = "Información del clima fuera de servicio	 ";
+                    }
+                }
+                if(data==="getWeatherTm()")
+                    try{
+                        data=await eval(this.chat_tree['message']);
+                        let condi=data.forecast.forecastday.condition.text;
+                        let condi_img=data.forecast.forecastday.condition.icon;
+                        let temp=data.forecast.forecastday.avgtemp_c;
+                        data = condi_img+"Condicion: "+condi+"<br>"+"Temperatura promedio: "+temp+"°C<br>";
+                    } catch (e) {
+                        data = "Información del clima fuera de servicio";
+                    }
             }
             resp += data;
             resp += "<br><br>Please input <b>Reset</b> to reset chat now";
@@ -147,26 +174,54 @@ class ChatTree {
     }
 }
 
-async function getJoke() {
+//e9712a2f5c974cd49876955e6d391db4 api Key
+async function getJokeSP() {
+    const response = await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single&lang=es');
+    const jsonResp = await response.json();
+    return jsonResp;
+}
+
+async function getJokeEN() {
     const response = await fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single');
     const jsonResp = await response.json();
     return jsonResp;
 }
 
-async function getNewsHN() {
-    const response = await fetch('https://api.mediastack.com/v1/news?access_key=2df46a1fd149c16f54c6d4b925ec1570&countries=hn&languages=es&limit=1');
+async function getNewsSC() {
+    const response = await fetch('https://newsapi.org/v2/top-headlines?category=science&apiKey=e9712a2f5c974cd49876955e6d391db4&language=es');
     const jsonResp = await response.json();
     return jsonResp;
 }
 
-async function getNewsCM() {
-    const response = await fetch('https://api.mediastack.com/v1/news?access_key=2df46a1fd149c16f54c6d4b925ec1570&countries=hn&languages=es&limit=1&keywords=comayagua');
+async function getNewsBC() {
+    const response = await fetch('https://newsapi.org/v2/top-headlines?q=bitcoin&apiKey=e9712a2f5c974cd49876955e6d391db4&language=es');
     const jsonResp = await response.json();
     return jsonResp;
 }
 
 async function getNewsQatar() {
-    const response = await fetch('https://api.mediastack.com/v1/news?access_key=2df46a1fd149c16f54c6d4b925ec1570&countries=hn&languages=es&limit=1&keywords=Qatar');
+    const response = await fetch('https://newsapi.org/v2/top-headlines?q=Qatar&apiKey=e9712a2f5c974cd49876955e6d391db4&language=es');
+    const jsonResp = await response.json();
+    return jsonResp;
+}
+
+async function getWeatherTd()
+{
+    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=5d108666afc144dba4b33110221112&q=Comayagua&aqi=yes');
+    const jsonResp = await response.json();
+    return jsonResp;
+}
+
+async function getWeatherNw()
+{
+    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=5d108666afc144dba4b33110221112&q=Comayagua&days=7&aqi=no&alerts=no');
+    const jsonResp = await response.json();
+    return jsonResp;
+}
+
+async function getWeatherTm()
+{
+    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=5d108666afc144dba4b33110221112&q=Comayagua&days=1&aqi=no&alerts=no');
     const jsonResp = await response.json();
     return jsonResp;
 }
